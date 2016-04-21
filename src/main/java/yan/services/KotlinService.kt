c75801f6d3b0af1e8c1884fa41312ce5ece98d9e
yan <alvindaiyan@gmail.com>
2016-04-22 10:47:33 +1200

    added kotlin dependency

---
+++ src/main/java/yan/services/KotlinService.kt
@@ -0,0 +1,27 @@
+package yan.services
+
+import com.google.gson.Gson
+import io.swagger.annotations.Api
+import javax.ws.rs.Path
+import javax.ws.rs.GET
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+
+
+/**
+ * Created by yan on 22/04/2016.
+ */
+@Path("/kotlin")
+@Api("kotlin")
+class KotlinService {
+
+    @GET
+    @Path("/{text}")
+    @Produces(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN)
+    fun kotlin(@PathParam("text") text:String) : Response {
+        var gson = Gson();
+        return Response.ok(gson.toJson(text)).build();
+    }
+}
